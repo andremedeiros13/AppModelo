17bfe31eda2c1784d3caba85dcaf106a39a21380
Andre <Andre@ANDRE-PC>
2022-12-23 02:54:48 -0300

    Add project files.

---
+++ src/Dev.UI.Site/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dev.UI.Site.Controllers
+{
+    public class HomeController: Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
